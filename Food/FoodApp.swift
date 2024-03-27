bbd5f67d052f196d6d6d8f7dd99fdd2bdefd9e55
mohammad <mohammadparwani@outlook.com>
2024-03-27 16:55:21 +0100

    Initial Commit

---
+++ Food/FoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodApp.swift
+//  Food
+//
+//  Created by mohammad on 27/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
